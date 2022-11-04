e042f86e25291db056fd2ebe8952269a46cbfeec
Sahariar Redwan <sahariarredwan5@gmail.com>
2022-11-04 21:02:03 +0600

    new

--- main.js
+++ main.js
@@ -1,15 +1,200 @@
+// var firstName="redwan"
+// var lastName="sahariar"
 
-function calclu(...number){
+// var totalName=(firstName+" "+lastName);
 
-    let sum=0;
-    for(let i of number){
-        sum=sum+i;
-    }
-console.log(sum)
+// console.log(totalName)
 
+
+// var nmbr=25.33
+
+//  var nm=parseFloat(nmbr)
+
+// console.log(nm)
+
+
+
+// var i;
+// for(i=0;i<=10;i=i+3){
+//     console.log("Allah")
+// }
+
+
+
+// var firstName=["sahariar" , "Alam" , "Redwan"]
+
+//     for(let oneName of firstName){
+
+//         console.log(oneName)
+
+//     }
+
+
+
+// var RedDetails={
+
+//     persnalDetails:{
+//         firstName:"redwan",
+//         lastName:"sahariar",
+//         roll:484318 ,
+//     },
+
+//     ContactInfo:{
+//         Upozila:"fulgazi" ,
+//         Zila:"Feni" ,
+//         PostOffice:"anansdapur"
+//     },
+
+//     study:{
+//         school:"Ideal Schol" ,
+//         college:"FCI"
+//     }
+
+// }
+
+// for(let props in RedDetails ){
+
+//     console.log([props][persnalDetails])
+// }
+
+
+// var redD={name:"redwan", roll:28}
+
+// for(let props in redD){
+//     console.log(props+'= '+redD[props])
+// }
+
+
+// function myfunc(){
+
+//     x=20;
+//     y=3;
+//     z=x+y;
+//     console.log(z)
+// }
+
+// myfunc()
+
+
+// function myFun(x,y){
+
+//     var z=x+y;
+//     console.log(z)
+
+
+// }
+
+// myFun(10,20)
+
+
+
+// function myfun(...a){
+
+//     console.log(a[5])
+
+// }
+
+// myfun(1,2,3,4,5,"a","b")
+
+
+
+// function funOne(){
+
+// //     return funTwo();
+
+// // }
+
+
+// // function funTwo(){
+
+// //     return 30;
+// // }
+
+// // console.log(funOne()+funTwo())
+
+
+
+// var playersName=["redwan","sahariar","alam"]
+
+// playersName.push("rihan");
+
+// console.log(playersName)
+
+
+// var playersName=["redwan","sahariar","alam"]
+
+// playersName.pop();
+
+// console.log(playersName)
+
+
+// var playersName=["redwan","sahariar","alam"]
+
+// playersName.shift();
+
+// console.log(playersName)
+
+
+
+
+// var playersName=["redwan","sahariar","alam"]
+
+// playersName.unshift("rihan");
+
+// console.log(playersName)
+
+
+
+// var nam=0;
+// while(nam<=50){
+//     if(nam%2==0){
+//         console.log(nam)
+//     }
+//     nam++
+// }
+
+
+
+// var num=["redwa",2,3,4,5,6,7,8,9]
+
+// for(let i=0;i<num.length;i++){
+
+//     console.log(num[i])
+
+    // if(num[i]>=8){
+    //     continue;
+    // }
+    // else{
+    //     console.log(num[i])
+    // }
+
+
+
+
+
+// // do {
+// 	var nam=["red","one","sahariar"]
+	
+// 	// for(let i=0;i<nam.length;i++){
+// 	//     console.log(nam[i])
+// 	// }
+// } while (condition);
+
+
+console.log(6+7)
+
+var num=[1,2,3,4,5,6,7,8,9,10]
+for(i=0;i<num.length;i++){
+    console.log(num[i])
 }
 
-calclu(1,2,3,4)
 
-var red
+// var nam= ()=>{
+//     return "02000"
+// }
+
+// console.log(nam())
+
+
+
 
